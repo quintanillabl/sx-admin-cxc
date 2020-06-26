bd53b0a0a209e0c6a397c8f645c41eb8b2c31ded
Ruben Cancino <rubencancino6@gmail.com>
2020-06-26 14:48:22 -0500

    Ajuste

--- src/app/_shared/components/footer/footer.component.ts
+++ src/app/_shared/components/footer/footer.component.ts
@@ -4,9 +4,23 @@ import { Component, OnInit } from '@angular/core';
   selector: 'sx-footer',
   template: `
     <div layout="row" layout-align="start center">
-      <span class="md-caption">Copyright &copy; 2017 Luxsoft Mx. All rights reserved</span>
+      <span class="md-caption"
+        >Copyright &copy; 2017 Luxsoft Mx. All rights reserved
+      </span>
+      <span class="fill"></span>
+      <span class="version"> Versión <strong>1.0.53</strong> </span>
     </div>
-  `
+  `,
+  styles: [
+    `
+      .fill {
+        flex: 1;
+      }
+      .version {
+        padding-rigt: 10px;
+      }
+    `,
+  ],
 })
 export class FooterComponent implements OnInit {
   constructor() {}
